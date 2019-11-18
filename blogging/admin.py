d8a0476b83ee1d60616b8d7bd8783f055e0be4e7
James Ward <james@Jamess-MacBook-Pro.local>
2019-11-17 22:11:52 -0800

    Module 7 assignment

--- blogging/admin.py
+++ blogging/admin.py
@@ -1,6 +1,25 @@
 from django.contrib import admin
 from blogging.models import Post, Category
 
+
+# Sub-classes of admin.ModelAdmin to customize how the
+# corresponding models are displayed in the admin interface
+class CategoryInline(admin.TabularInline):
+    """
+    Adds a Category element to a class of admin.ModelAdmin
+    """
+    model = Category.posts.through
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    # Removes the UI element from the category admin page which allows posts to be associated with the category
+    exclude = ('posts',)
+
+
+class PostAdmin(admin.ModelAdmin):
+    inlines = [CategoryInline]
+
+
 # Registrations of DB models used by the blogging application
-admin.site.register(Post)
-admin.site.register(Category)
+admin.site.register(Post, PostAdmin)
+admin.site.register(Category, CategoryAdmin)
